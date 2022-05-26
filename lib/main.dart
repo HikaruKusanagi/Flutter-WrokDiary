1beca5243600d8865489a1ab0d3d7fd0349ca7cf
HikaruKusanagi <hikarugp24@gmail.com>
2022-05-26 23:59:52 +0900

    no message

--- lib/main.dart
+++ lib/main.dart
@@ -101,6 +101,9 @@ class _MyHomePageState extends State<MyHomePage> {
               '',
             ),
             Text(
+              Text(
+                '',
+              ),
               '$_counter',
               style: Theme.of(context).textTheme.headline4,
             ),
